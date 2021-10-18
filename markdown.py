fba6eb4a507b654955903d9ffee3ed497c49f6e7
Nidhin Radh P V <nidhin.radh.ra@gmail.com>
2021-10-18 12:52:09 +0530

    updated structure

---
+++ markdown.py
@@ -0,0 +1,9 @@
+class Markdown:
+    hide_streamlit_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                .block-container: {padding-top:10px;}
+                </style>
+                """
+markdown = Markdown()
