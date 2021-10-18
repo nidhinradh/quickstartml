037e13b8cf89214d8ef64cba98e3f6b296e98550
Nidhin Radh P V <nidhin.radh.ra@gmail.com>
2021-10-18 12:57:01 +0530

    Added env file

--- app/markdown.py
+++
@@ -1,9 +0,0 @@
-class Markdown:
-    hide_streamlit_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                .block-container: {padding-top:10px;}
-                </style>
-                """
-markdown = Markdown()
