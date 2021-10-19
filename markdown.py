2fb60f6acaa64e5e310c8144e0e5c96b2dc957d5
Nidhin Radh P V <nidhin.radh.ra@gmail.com>
2021-10-19 18:18:33 +0530

    Removed unwanted file

--- markdown.py
+++
@@ -1,8 +0,0 @@
-class Markdown:
-    hide_streamlit_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                </style>
-                """
-markdown = Markdown()
