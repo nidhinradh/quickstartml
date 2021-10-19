8228807e38c8f318b66e2541f53d49fdb5503bf6
Nidhin Radh P V <nidhin.radh.ra@gmail.com>
2021-10-19 11:08:29 +0530

    Change default theme

--- setup.sh
+++ setup.sh
@@ -8,5 +8,11 @@ enableCORS = true\n\
 serverAddress = \"quickstartml.herokuapp.com\"\n\
 gatherUsageStats = true\n\
 serverPort = $PORT\n\
+[theme]\n\
+primaryColor = \"#F63366\"\n\
+backgroundColor = \"#FFFFFF\"\n\
+secondaryBackgroundColor = \"#F0F2F6\"\n\
+textColor = \"#262730\"\n\
+font = \"sans serif\"\n\
 \n\
 " > ~/.streamlit/config.toml
